543bdf2da0b0c8c1d5e6c4ed98e52175ed5c516a
Tomek <quadility.app@gmail.com>
2018-04-18 22:38:13 +0200

    QUAD-03 JMS connect

--- src/main/java/com/thobho/quadilityr/api/handlers/ErrorHandler.java
+++
@@ -1,4 +0,0 @@
-package com.thobho.quadilityr.api.handlers;
-
-public class ErrorHandler {
-}
--- src/main/java/com/thobho/quadilityr/dataaccess/MeasurementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thobho.quadilityr.dataaccess;
-
-import com.thobho.quadilityr.dataaccess.entities.MeasurementEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface MeasurementRepository extends ReactiveMongoRepository<MeasurementEntity, String> {
-
-}
